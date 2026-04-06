a25ed03a8d35d714868689fe7b88100422668bba
belinzenewtone <newtonebelinzeojing@gmail.com>
2026-04-06 12:17:28 +0300

    chore: release OTA v1.2.29 (40)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,8 @@ android {
         applicationId = "com.personal.lifeOS"
         minSdk = 26
         targetSdk = 35
-        versionCode = 39
-        versionName = "1.2.28"
+        versionCode = 40
+        versionName = "1.2.29"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 

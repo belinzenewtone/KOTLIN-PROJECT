8a1d8da978c3edaea9bb496bde75e10007adbfa8
Belinze Newtone <belinze.newtone@jtl.co.ke>
2026-04-17 09:01:40 +0300

    chore: publish signed OTA release v1.2.36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,8 @@ android {
         applicationId = "com.personal.lifeOS"
         minSdk = 26
         targetSdk = 35
-        versionCode = 46
-        versionName = "1.2.35"
+        versionCode = 47
+        versionName = "1.2.36"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
